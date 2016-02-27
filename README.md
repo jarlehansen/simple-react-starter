58872717d68517ce09ab8ef4c2fc3881707a5acb
jarlehansen <jarle@jarlehansen.net>
2016-02-27 13:13:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Simple React Starter
+
+A very simple react starter kit containing:
+* webpack - https://webpack.github.io/
+* react - https://facebook.github.io/react/
+* babel - https://babeljs.io/
+* html-webpack-plugin - https://github.com/ampedandwired/html-webpack-plugin
+
+## Installation
+```
+npm install
+```
+
+## Usage
+Webpack dev server (default port = 8080)
+```
+npm start
+```
+
+Build
+```
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var config = {
+  devtool: 'source-map',
+  entry: __dirname + '/src/App.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.tmpl.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist',
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  }
+};
+
+module.exports = config;
